ba47aeb73e8b999fcfad5d6e36726bc5a27dcadb
Land Lu <v-luting@microsoft.com>
2018-06-13 15:59:09 +0800

    Add project files.

---
+++ CustomEffectDemo/CustomEffectDemo.iOS/FocusEffect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CustomEffectDemo.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(FocusEffect), "FocusEffect")]
+namespace CustomEffectDemo.iOS
+{
+    public class FocusEffect : PlatformEffect
+    {
+
+        UIScrollView nativeControl;
+        private bool _isAttached;
+        protected override void OnAttached()
+        {
+            nativeControl = Control as UIScrollView;
+            if (nativeControl != null && !_isAttached)
+            {
+                nativeControl.Scrolled += NativeControl_Scrolled;
+                _isAttached = true;
+            }
+        }
+
+        private void NativeControl_Scrolled(object sender, EventArgs e)
+        {
+            //var command = ScrollingEffect.GetCommand(Element);
+            //command?.Execute(null);
+        }
+        protected override void OnDetached()
+        {
+            if (_isAttached)
+            {
+                nativeControl.Scrolled -= NativeControl_Scrolled;
+                _isAttached = false;
+            }
+        }
+    }
+}
